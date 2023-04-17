b9f89e1fd4a88e849a076a5547e869cbd70ca469
Rhealyn Joy Macaraig <100116890+mrhealyn@users.noreply.github.com>
2023-04-17 09:50:55 +0800

    second commit

--- src/main/java/com/example/newWiki/repository/WikiRepo.java
+++ src/main/java/com/example/newWiki/repository/WikiRepo.java
@@ -1,6 +1,8 @@
 package com.example.newWiki.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +11,8 @@ import com.example.newWiki.model.Wiki;
 @Repository
 public interface WikiRepo extends JpaRepository<Wiki, Long>{
 
-	
-	
-	
+	Wiki findByTopic(String topic);
+
+	Optional <Wiki> findByTitle(String title);
 }
 
--- src/test/java/com/example/newWiki/NewWikiApplicationTests.java
+++ src/test/java/com/example/newWiki/NewWikiApplicationTests.java
@@ -1,13 +1,80 @@
 package com.example.newWiki;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.example.newWiki.model.Wiki;
+import com.example.newWiki.repository.WikiRepo;
+
 
 @SpringBootTest
 class NewWikiApplicationTests {
+	
+
+	@Autowired
+	 WikiRepo wikiRepo;
+	
 
 	@Test
-	void contextLoads() {
+	@Rollback(false)
+	public void testCreate () {
+		Wiki wiki = new Wiki(null, "brain", "abc", "xyz");
+		Wiki savedWiki = wikiRepo.save(wiki);
+		assertNotNull(savedWiki);
+	}
+	
+	
+	@Test
+	public void testFindAll () {
+		List list = wikiRepo.findAll();
+		assertThat(list).size().isGreaterThan(0);
+	}
+		
+	@Test
+	public void testFindWikiByTopicExist () {
+		String topic = "abc";
+		Wiki wiki = wikiRepo.findByTopic(topic);
+		assertThat(wiki.getTopic()).isEqualTo(topic);
+	}
+	
+	@Test
+	public void testFindWikiByTopicNotExist () {
+		String topic = "not";
+		Wiki wiki = wikiRepo.findByTopic(topic);
+		assertNull(wiki);
+	}
+		
+	private void assertNull(Wiki wiki) {
+				
 	}
 
+
+	@Test
+	public void testUpdate() {
+		Wiki wiki = wikiRepo.findById(502L).get();
+		wiki.setTopic("new topic");
+		wikiRepo.save(wiki);
+		assertNotEquals("existing topic", wikiRepo.findById(502L).get().getTopic());
+	}
+		
+	@Test
+	public void testDelete () {
+		wikiRepo.deleteById(1L);
+		assertThat(wikiRepo.existsById(1L)).isFalse();
+	}
 }
+	
+	
+	
+	
+
